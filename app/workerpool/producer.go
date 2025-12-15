28e3c312f45652bc4eb1d51e51270a3f84740712
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-12-15 17:21:04 +0800

    Remove test producer

--- app/workerpool/producer.go
+++ app/workerpool/producer.go
@@ -1,31 +1,7 @@
 package workerpool
 
-import "fmt"
-
 type Producer interface {
 	Produce() (Job, bool)
 }
 
 type TestProducer struct{}
-
-func (p *TestProducer) Produce() (Job, bool) {
-	return Job{
-		Action: func(a Args) Result {
-
-			num, exists := a[0].(int)
-
-			if !exists {
-				return Result{
-					Payload: nil,
-					Error:   fmt.Errorf("obj in channel not a number"),
-				}
-			}
-
-			return Result{
-				Payload: num + 1,
-				Error:   nil,
-			}
-		},
-		Args: []interface{}{1},
-	}, true
-}
